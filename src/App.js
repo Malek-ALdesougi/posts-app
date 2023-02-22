caeb085cd29453d65d92d6405674e315aafb07cb
Malek AL-Desougi <malekaldesougi@gmail.com>
2023-02-22 09:46:45 +0300

    I have did some refactore to the app component and finish the edit in the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import style from './app.module.css'
 import NavBar from './components/navbar';
 import Home from './components/Home';
 import { Routes, Route } from 'react-router-dom';
 import NewPost from './components/NewPost/index';
 import SingleBlog from './components/SingleBlog';
 import NotFound from './components/NotFound/NotFound';
-import Search from './components/Search';
 import Login from './components/Login';
 // send this variable with use context 
 
@@ -20,13 +19,13 @@ import Login from './components/Login';
 function App() {
 
   return (
-    <div className='malek' style={{ width: '800px', margin: '0 auto' }}>
+    <div className={style.app}>
+      <h1 className={style.malek}>malek malek malek</h1>
       <NavBar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/new-post' element={<NewPost/>} />
         <Route path='/single-blog/:id' element={<SingleBlog />}/>
-        <Route path='/search' element={<Search />} />
         <Route path='/login' element={<Login />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,66 +1,50 @@
 import style from './style.module.css'
 import useFetch from '../../Hooks/useFetch';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import { MDBIcon } from 'mdb-react-ui-kit';
 
 const Home = () => {
 
     const { posts, loading, error } = useFetch('http://localhost:8000/posts');
-    const searchRef = useRef('');
-    const navigate = useNavigate();
-    // first thing do useRef with empty array
-
     //TODO: i need to create a ref with an initial value of array to be able to access each component separatly
-    const containerDivRef = useRef([]);
     const authorsNamesRef = useRef([]);
     const inputRef = useRef([]);
     const [search, setSearch] = useState('');
-    
+    const [isEditable, setIsEditable] = useState({});
+    const [newValue, setNewValue] = useState({});
+
     if (loading !== false) return;
     if (error) return;
 
-    // this is the second way of solution for searching by name ;
     const handleSearchInputChnage = (e) => {
         setSearch(e.target.value);
-        searchRef.current = e.target.value;
-    }
-    const handleSearchBtn = async () => {
-        //   const data = await  fetch(`http://localhost:8000/posts/?q=${searchRef.current}`);
-        //   const result = await data.json();
-        //   navigate('search', { state: result });
     }
 
-    const changeAuthorNameIcon = (index) => {
-
-        containerDivRef.current[index].style.display = 'block';
-        authorsNamesRef.current[index].style.display = 'none';
-        inputRef.current[index].value = authorsNamesRef.current[index].textContent;
-        // I need to take this name value to put it inside te input 
-        console.log(authorsNamesRef.current[index].textContent);
+    const changeEditState = (index) => {
+        setIsEditable({ ...isEditable, [`${index}`]: true });
     }
 
     const handleEditAuthorName = (index) => {
 
-        console.log(index);
-        let editInputValue = inputRef.current[index]?.value ;
-        authorsNamesRef.current[index].textContent = editInputValue;
-        containerDivRef.current[index].style.display = 'none';
-        authorsNamesRef.current[index].style.display = 'block';
-
-
+        // if new value is there set it to the newValue state 
+        // to be able to read to the input value and updated
+        let editInputValue = inputRef.current[index].value;
+        setNewValue({ ...newValue, [`${index}`]: editInputValue });
+        setIsEditable({ ...isEditable, [`${index}`]: false })
     }
 
-    const handleCancelAuthorName = (index) => {
-        containerDivRef.current[index].style.display = 'none';
+    const changeEditStateOpposite = (index) => {
+        setIsEditable({ ...isEditable, [`${index}`]: false })
     }
+
+
 
     return (
 
         <>
             <div className={style.container2}>
                 <input onChange={handleSearchInputChnage} type={'text'} placeholder={'Search a Blog'}></input>
-                {/* <button onClick={handleSearchBtn} className='btn' type='button'>Search Blog</button> */}
             </div>
 
             {loading ? 'Loading ...' :
@@ -70,20 +54,24 @@ const Home = () => {
                     {posts?.filter(item => search ? item.author.toLowerCase().includes(search.toLowerCase()) : item.author).map((blog, index) => {
                         return (
                             <div className={style?.blogs} key={blog?.id}>
-                                <MDBIcon className={style.editIcon} fas icon="pen-square" onClick={() => changeAuthorNameIcon(index)} />
-                                <h3 ref={name => authorsNamesRef.current[index] = name}>{blog?.author}</h3>
-                                    {/* this div is made multiple times useRef not working */}
-                                <div style={{display: 'none'}} ref={el => containerDivRef.current[index] = el}>
-                                    <input ref={input => inputRef.current[index] = input} type={'text'} />
-                                    <button onClick={() => handleEditAuthorName(index)} type='button'>Edit</button>
-                                    <button onClick={() => handleCancelAuthorName(index)} type='button'>Cancel</button>
-                                </div>
+
+                                {!isEditable[index] ?
+                                    <>
+                                        <MDBIcon className={style.editIcon} fas icon="pen-square" onClick={() => changeEditState(index)} />
+                                        <h3>{newValue[index] ? newValue[index] : blog?.author}</h3>
+                                    </> :
+                                    <div>
+                                        <input defaultValue={newValue[index] ? newValue[index] : blog?.author} ref={(input) => inputRef.current[index] = input} type={'text'} />
+                                        <button onClick={() => handleEditAuthorName(index)} type='button'>Edit</button>
+                                        <button onClick={() => changeEditStateOpposite(index)} type='button' >Cancel</button>
+                                    </div>
+                                }
                                 <Link to={`single-blog/${blog?.id}`} className={style.link}>
                                     <p>{blog?.content}</p>
                                 </Link>
 
 
-                                
+
                                 ------
                             </div>
                         )
